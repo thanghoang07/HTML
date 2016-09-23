3425f83ffb614d2d938df035034304b765abad6f
Hoàng Đức Thắng <thanghoang07@outlook.com>
2016-09-23 19:27:54 +0700

    thanghoang07
    code html

---
+++ js/myScript2.js
@@ -0,0 +1,28 @@
+/*Trong notFound*/
+function NF1(){
+    window.location.href = '../index.html';
+}
+/*trong phần bài tập*/
+function t1(){
+    window.location.href = '../tuan/Tuan1.html';
+}
+function t2(){
+    window.location.href = '../tuan/Tuan2.html';
+}
+/*Trong tuan*/
+function myFunction(){
+    //alert('Tải về máy');
+    window.location.href = '../baiTap/tuan1/Bai1.docx';
+}
+function myFunction2(){
+    //alert('Xem online');
+        window.location.href = 'https://goo.gl/DRT2yK';
+}
+function myFunction3(){
+    alert('Tải về máy');
+    window.location.href = '../notFound.html';
+}
+function myFunction4(){
+    alert('Xem online');
+    window.location.href = '../notFound.html';
+}
